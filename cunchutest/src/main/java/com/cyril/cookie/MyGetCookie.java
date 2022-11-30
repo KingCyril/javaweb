03ddce3ab6751e936295995aa45c42e79ceba454
spf01137625 <spf01137625@cainiao.com>
2022-11-30 18:46:18 +0800

    sa

--- cunchutest/src/main/java/com/cyril/cookie/MyGetCookie.java
+++ cunchutest/src/main/java/com/cyril/cookie/MyGetCookie.java
@@ -1,2 +1,23 @@
-package com.cyril.cookie;public class MyGetCookie {
+package com.cyril.cookie;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/demo2")
+public class MyGetCookie extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        Cookie[] cookies = req.getCookies();
+
+        for (Cookie cookie : cookies) {
+           if("username".equals(cookie.getName())){
+               System.out.println(cookie.getValue());
+           }
+        }
+    }
 }
--- cunchutest/src/main/java/com/cyril/cookie/MySetCookie.java
+++ cunchutest/src/main/java/com/cyril/cookie/MySetCookie.java
@@ -1,2 +1,18 @@
-package com.cyril.cookie;public class MySetCookie {
+package com.cyril.cookie;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/demo1")
+public class MySetCookie extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        Cookie cookie = new Cookie("username", "spf");
+        resp.addCookie(cookie);
+    }
 }
